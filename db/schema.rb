4ca0c12e9471de17f2bd6b786fdadb3ec261204d
yorthehunter <boru.design@gmail.com>
2015-02-21 16:25:17 -0500

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141107154847) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: true do |t|
     t.string   "purchase_date"
     t.string   "game_name"
