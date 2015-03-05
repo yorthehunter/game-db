27bd8fe1e1657bc9e3a5b6f552d65ace8ce4f3e7
yorthehunter <boru.design@gmail.com>
2015-03-05 18:10:03 -0500

    migration

---
+++ db/migrate/20150305224016_add_not_available_flag.rb
@@ -0,0 +1,4 @@
+class AddNotAvailableFlag < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226023622) do
+ActiveRecord::Schema.define(version: 20150305224153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150226023622) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "purchase_price"
+    t.boolean  "not_available"
   end
 
 end
