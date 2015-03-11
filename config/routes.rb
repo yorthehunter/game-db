f446cb11089c489cb36fe4d4a3c2a7b11124d0b1
yorthehunter <boru.design@gmail.com>
2015-03-10 21:23:24 -0400

    i forget all of what is in this commit. Suffice to say, it seems to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  match "/activate" => "games#activate", :via => [:get], as: :activate
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
