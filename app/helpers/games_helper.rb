18041e07205e6fdafc4ebc7a24597ad7e3ac7437
yorthehunter <boru.design@gmail.com>
2015-03-14 20:51:41 -0400

    bigger buttons, better colors; use helper to search Price Charting; use alt name for searching

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,4 +4,9 @@ module GamesHelper
     params[:action] == action ? "disabled" : false
   end
 
+  def search_link(game)
+    name = game.game_alt_name ? game.game_alt_name : game.game_name
+    "http://videogames.pricecharting.com/search?q=#{name}&type=videogames&go=Go"
+  end
+
 end
